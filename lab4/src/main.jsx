6284110f7059fcf70580f32ee0cef218fc26c142
Vladyslav Shtop <shtop.vlad@gmail.com>
2025-03-25 19:15:21 +0200

    Deploy lab4 4

--- lab4/src/main.jsx
+++ lab4/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
 
+// Use window.location.pathname to handle base path dynamically
+const basePath = window.location.pathname.split('/')[1] || ''
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <App basename={`/${basePath}`} />
+  </React.StrictMode>
 )
